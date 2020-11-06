e5ad5f145bbdfef8da2b8049695c38e2f43f7c1f
anst9000 <coltlaboy@yahoo.se>
2020-11-06 10:11:58 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { AutofocusFixModule } from 'ngx-autofocus-fix';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+
+@NgModule({
+  declarations: [AppComponent, TodoListComponent, TodoItemComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    AutofocusFixModule.forRoot(),
+    BrowserAnimationsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Todo } from 'src/app/interfaces/todo';
+import { TodoService } from 'src/app/services/todo.service';
+
+@Component({
+  // tslint:disable-next-line: component-selector
+  selector: 'todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.scss'],
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
+
+  constructor(public todoService: TodoService) {}
+
+  ngOnInit(): void {}
+}
